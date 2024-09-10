dafc3a1b8c04027292bd1a5082d719d77790b988
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2024-09-10 15:09:32 +0200

    test

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Hauptseite - Home',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': True
+    }
+    
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
